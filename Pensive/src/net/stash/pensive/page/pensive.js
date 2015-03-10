9a7f675b83dac5d8087cb4fb9b247ad0962b7937
Tom Parker <tparker@usgs.gov>
2015-03-10 12:45:26 -0800

    Mid-day checkpoint. Lots of good stuff, work continues.

--- Pensive/src/net/stash/pensive/page/pensive.js
+++ Pensive/src/net/stash/pensive/page/pensive.js
@@ -1,5 +1,6 @@
 var refreshPeriodMS = ${refreshPeriod} * 1000;
 var timeFormatter = new SimpleDateFormat("HH:mm");
+var dateFormatter = new SimpleDateFormat("d MMMM, yyyy");
 var pathFormatter = new SimpleDateFormat("${filePathFormat}");
 var fileFormatter = new SimpleDateFormat("${fileSuffixFormat}");
 
@@ -9,17 +10,35 @@ var rowSpanMS;
 var mosaicSpanMS;
 
 function init() {
+	console.trace();
 	rowSpanMS = 60 * 60 * 1000;
 	mosaicSpanMS = 180 * 60 * 1000;
 	endTime = new Date(getMostRecentEnd());
 	startTime = new Date(endTime.getTime() - (refreshPeriodMS));
-	
-	registerEventHandlers();
 
-	updateTime();
+	parseParameters();	
+	registerEventHandlers();
+	$("#subnet").trigger("change");	
+
+	updateTimeLabel();
 	updateMosaic();
+	
+	/* leanModal init */
+	$('form').submit(function(e){return false;});
+    $("a[rel*='leanModal']").leanModal({ top: 110, overlay: 0.75, closeButton: ".hidemodal" });
 }
 
+function parseParameters() {
+	var param = getUrlParameter("rowSpan");
+	if ($.isNumeric(param)) {
+		rowSpanMS = param * 60 * 1000;
+	}
+
+	param = getUrlParameter("mosaicSpan");
+	if ($.isNumeric(param)) {
+		mosaicSpanMS = param * 60 * 60 * 1000;
+	}	
+}
 
 function registerEventHandlers() {
 	$("#nextSubnet").on('click', {step: 1}, incrementSubnet);
@@ -27,23 +46,11 @@ function registerEventHandlers() {
 	$("#currentImage").on('click', {step: (Math.pow(2,32) - 1)}, incrementTime);
 	$("#nextImage").on('click', {step: 1}, incrementTime);
 	$("#previousImage").on('click', {step: -1}, incrementTime);
-
+	$("#mosaicButton").on('click', updateMosaic);
 	$("#subnet").on('change', updateSubnet);
-	$("#subnet").trigger("change");	
 	
-	jQuery('.positiveInt').keyup(function () { 
-	    this.value = this.value.replace(/[^0-9]/g,'');
-	});
-}
-
-function displayMosaicOptions() {
-	vex.dialog.confirm({
-		  message: 'Are you absolutely sure you want to destroy the alien planet?',
-		  callback: function(value) {
-		    return console.log(value ? 'Successfully destroyed the planet.' : 'Chicken.');
-		  }
-		});
-}
+	$(".positiveInt").on('keyup', function () {this.value = this.value.replace(/[^0-9]/g,'');});
+	}
 
 /* The subnet changed, now what? */
 function updateSubnet() {
@@ -52,26 +59,13 @@ function updateSubnet() {
 	updateMosaic();
 }
 
-/* The subnet changed, now what? */
-function updateMosaicSpan() {
-	var span=$("#mosaicSpan option:selected").val();
-	mosaicSpanMS = span * 60 * 60 * 1000;
-	updateMosaic();
-}
-
-/* The subnet changed, now what? */
-function updateMosaicRowSpan() {
-	var span=$("#mosaicRowSpan").val();
-	rowSpanMS = span * refreshPeriodMS;
-	updateMosaic();
-}
-
 function updateMosaicOptions() {
-	var span=$("#mosaicSpan option:selected").val();
-	mosaicSpanMS = span * 60 * 60 * 1000;
+	var spanMS=$("#mosaicSpan").val()  * 60 * 60 * 1000;
+	mosaicSpanMS = spanMS;
 
-	var span=$("#mosaicRowSpan").val();
-	rowSpanMS = span * refreshPeriodMS;
+	spanMS=$("#mosaicRowSpan").val() * 60 * 1000;
+	rowSpanMS = refreshPeriodMS * Math.ceil(spanMS / refreshPeriodMS);
+	
 	updateMosaic();
 
 }
@@ -86,8 +80,8 @@ function incrementSubnet(e) {
 }
 
 /* The time changed, now what? */
-function updateTime() {
-		$("#timeSpan").text(timeFormatter.format(startTime) + " - " + timeFormatter.format(endTime));
+function updateTimeLabel() {
+		$("#timeSpan").text(dateFormatter.format(startTime) + " " + timeFormatter.format(startTime) + " - " + timeFormatter.format(endTime) + " UTC");
 }
 
 function incrementTime(e) {
@@ -116,64 +110,97 @@ function getMostRecentEnd() {
 
 
 function updateMosaic() {
+	$("#mosaicButton").hide();
+	$("#mosaicOptionsButton").show();
+
+	var network = $("#network option:selected").text();
+	var subnet = $("#subnet option:selected").text();
+
 	var frame = $("#mainFrame");
+	frame.empty();
 
 	var mosaicEndMS = endTime.getTime();
 	var mosaicStartMS = mosaicEndMS - mosaicSpanMS;
-	
 	startTime.setTime(mosaicStartMS);
-	updateTime();
-	
-	frame.empty();
+	updateTimeLabel();
+
 	var table = $(document.createElement('table'));
-	table.addClass('center');
 	frame.append(table);
+	table.addClass('center');
 
 	var rowStartMS = mosaicStartMS;
-	while (rowStartMS <= mosaicEndMS) {
+	while (rowStartMS < mosaicEndMS) {
+		var cell;
+		
 		var rowStart = new Date(rowStartMS);
 		var rowEndMS = rowStartMS + rowSpanMS;
-		var row = $(document.createElement('tr'));
-		row.addClass("mosaic");
-		table.append(row);
-		var cell = $(document.createElement('td'));
-		cell.addClass("mosaicTitle");
-		cell.text(timeFormatter.format(rowStart));
-		row.append(cell);
-		var cellEndMS = rowStart.getTime() + refreshPeriodMS; 
 
+		var row = $(document.createElement('tr'));
+		table.append(row);
+		row.addClass("mosaic");
+
+		cell = $(document.createElement('td'));
+		row.append(cell);
+		cell.addClass("mosaicTitle");
+		cell.html(timeFormatter.format(rowStart) + " <span class=\"small\">UTC</span>");
+
+		var cellEndMS = rowStart.getTime() + refreshPeriodMS; 
 		while (cellEndMS <= rowEndMS) {
 			cell = $(document.createElement('td'));
-			cell.addClass("mosaic");
 			row.append(cell);
-			var image = $(document.createElement('img'));
-			cell.append(image);
-			
-			var network = $("#network option:selected").text();
-			var subnet = $("#subnet option:selected").text();
+			cell.addClass("mosaic");
+
 			var cellEnd = new Date(cellEndMS);
 			var url = network + "/" + subnet + "/" + pathFormatter.format(cellEnd) + "/" + subnet + fileFormatter.format(cellEnd) + "_thumb.png";
+
+			var image = $(document.createElement('img'));
+			cell.append(image);
 			image.attr('src', url);
+			image.on('click', {endTime: cellEnd}, displayPlot);
+			
 			cellEndMS += refreshPeriodMS;
 		}		
 		
 		cell = $(document.createElement('td'));
-		cell.addClass("mosaicTitle");
-		cell.text(timeFormatter.format(new Date(rowEndMS)));
 		row.append(cell);
+		cell.addClass("mosaicTitle");
+		cell.html(timeFormatter.format(new Date(rowEndMS)) + " <span class=\"small\">UTC</span>");
 		
 		rowStartMS += rowSpanMS;
 	}
 }
 
+function displayPlot(e) {
+	$("#mosaicButton").show();
+	$("#mosaicOptionsButton").hide();
 
+	endTime = e.data.endTime;
+	startTime.setTime(endTime - refreshPeriodMS);
+	updateTimeLabel();
+	
+	var frame = $("#mainFrame");
+	frame.empty();
 
+	var image = $(document.createElement('img'));
+	frame.append(image);
 
+	var network = $("#network option:selected").text();
+	var subnet = $("#subnet option:selected").text();
+	var url = network + "/" + subnet + "/" + pathFormatter.format(endTime) + "/" + subnet + fileFormatter.format(endTime) + ".png";
+	image.attr('src', url);
+}
 
-
-
-
-
-
-
-
+/* http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html */
+function getUrlParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}   
