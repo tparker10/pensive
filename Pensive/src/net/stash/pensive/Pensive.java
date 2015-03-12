58dcc916f905403d002fc41d4c2d3acfc1ea409e
Tom Parker <tparker@usgs.gov>
2015-03-11 16:13:43 -0800

    Remove prunned networks and subnets from config

--- Pensive/src/net/stash/pensive/Pensive.java
+++ Pensive/src/net/stash/pensive/Pensive.java
@@ -94,7 +94,7 @@ public class Pensive {
                 networkIt.remove();
                 continue;
             }
-
+            
             Iterator<String> subnetIt = subnets.iterator();
             while (subnetIt.hasNext()) {
                 String subnet = subnetIt.next();
@@ -111,7 +111,9 @@ public class Pensive {
                 LOGGER.log(Level.INFO, "Assigning subnet " + subnet + " to " + dataSource);
                 scheduler.add(new SubnetPlotter(network, subnet, subnetConfig));
             }
+            netConfig.putList("subnet", subnets);
         }
+        configFile.putList("network", networks);
 
         Iterator<String> schedulerIt = plotScheduler.keySet().iterator();
         while (schedulerIt.hasNext()) {
@@ -180,115 +182,4 @@ public class Pensive {
         Pensive pensive = new Pensive(cf);
         pensive.schedulePlots();
     }
-    
-    
-    
-//	/**
-//	 * Assign subnets to it wave server
-//	 */
-//	private void assignSubnets() {
-//		List<String> networks = configFile.getList("network");
-//		if (networks == null)
-//			throw new RuntimeException("No network directives found.");
-//
-//		Iterator<String> networkIt = networks.iterator();
-//		while (networkIt.hasNext()) {
-//			String network = networkIt.next();
-//			ConfigFile netConfig = configFile.getSubConfig(network, true);
-//			List<String> subnets = netConfig.getList("subnet");
-//			if (subnets == null) {
-//				LOGGER.log(Level.WARNING, "No subnet directives for network " + network + " found. Skipping.");
-//				networkIt.remove();
-//				continue;
-//			}
-//
-//			Iterator<String> subnetIt = subnets.iterator();
-//			while (subnetIt.hasNext()) {
-//				String subnet = subnetIt.next();
-//				ConfigFile subnetConfig = netConfig.getSubConfig(subnet, true);
-//				if (subnetConfig.getList("channel") == null) {
-//					LOGGER.log(Level.WARNING, "No channel directives for subnet " + subnet + " found. Skipping.");
-//					subnetIt.remove();
-//					continue;
-//				}
-//
-//				String dataSource = subnetConfig.getString("dataSource");
-//				PlotScheduler scheduler = plotScheduler.get(dataSource);
-//				LOGGER.log(Level.INFO, "Assigning subnet " + subnet + " to " + dataSource);
-//				scheduler.add(new SubnetPlotter(network, subnet, subnetConfig));
-//			}
-//		}
-//	}
-//
-//	/**
-//	 * Prune PlotSchedulers that have no subnets assigned to them.
-//	 */
-//	public void pruneSchedulers() {
-//		Iterator<String> schedulerIt = plotScheduler.keySet().iterator();
-//		while (schedulerIt.hasNext()) {
-//			String server = schedulerIt.next();
-//			PlotScheduler ps = plotScheduler.get(server);
-//			if (ps.subnetCount() < 1) {
-//				LOGGER.log(Level.WARNING, "No subnets feeding from " + ps.name + ". I'll prune it.");
-//				schedulerIt.remove();
-//			}
-//		}
-//
-//	}
-//
-//	/**
-//	 * Schedule a recurring call to produce plot jobs.
-//	 */
-//	private void schedulePlots() {
-//		ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-//		for (PlotScheduler ps : plotScheduler.values()) {
-//			scheduler.scheduleAtFixedRate(ps, 0, SubnetPlotter.DURATION_S, TimeUnit.SECONDS);
-//		}
-//	}
-//
-//	/**
-//	 * Where it all begins
-//	 * 
-//	 * @param args
-//	 */
-//	public static void main(String[] args) {
-//
-//		// create the package-level logger so inheritance works
-//		Log.getLogger("gov.usgs");
-//		LogManager.getLogManager().getLogger("gov.usgs").setLevel(Level.FINEST);
-//
-//		Log.attachFileLogger(LOGGER, "pensiveLog", 100000, 10, true);
-//		LOGGER.setLevel(Level.INFO);
-//		LOGGER.finest("starting Pensive");
-//
-//		ConfigFile cf = new ConfigFile("pensive.config");
-//		if (!cf.wasSuccessfullyRead())
-//			throw new RuntimeException("Error reading config file " + cf);
-//
-//		if (cf.getList("debug") != null) {
-//			for (String name : cf.getList("debug")) {
-//				Logger l = Log.getLogger(name);
-//				l.setLevel(Level.ALL);
-//				LOGGER.fine("debugging " + name);
-//			}
-//		}
-//
-//		Pensive pensive = new Pensive(cf);
-//		pensive.schedulePlots();
-//	}
-//=======
-////    /**
-////     * Is this worth the trouble? Why not just always call the replaceAll()?
-////     * 
-////     * @param path
-////     *            filesystem path to convert
-////     * @return The path suitable for use in a URL
-////     */
-////    public static String pathToPath(String path) {
-////        if (File.separator.equals("/"))
-////            return path;
-////        else
-////            return path.replaceAll("/", Matcher.quoteReplacement(File.separator));
-////    }
-//>>>>>>> 273f1cb Begin HTML work.
 }
